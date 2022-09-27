ca90bddcb5b16036af12d4f99833c20a2e771fbf
nadaelnagar1 <33862156+nadaelnagar1@users.noreply.github.com>
2022-09-27 20:52:23 +0200

    initial

---
+++ txtstyle.js
@@ -0,0 +1,41 @@
+function ChangeFont(font){
+    document.getElementById('PAR').style.fontFamily = font;
+
+}
+
+function ChangeAlign(align){
+    document.getElementById('PAR').style.textAlign = align;
+
+}
+
+function ChangeHeight(height){
+    document.getElementById('PAR').style.lineHeight = height;
+    
+}
+
+function ChangeLSpace(space){
+    document.getElementById('PAR').style.letterSpacing = space;
+    
+}
+
+function ChangeIndent(indent){
+    document.getElementById('PAR').style.textIndent = indent;
+
+}
+
+function ChangeTransform(textTransform){
+    document.getElementById('PAR').style.textTransform =textTransform;
+}
+
+function ChangeDecorate(textDecoration){
+    document.getElementById('PAR').style.textDecoration = textDecoration;
+}
+
+function ChangeBorder(textBorder){
+   document.getElementById('PAR').style.border =textBorder;
+}
+
+function ChangeBorderColor(borderColor){
+   document.getElementById('PAR').style.borderColor =borderColor;
+
+}
